6e4d7078f49b8375781f19d564536a322b0abb33
Ishan-Agrawal1 <agrawalishan47@gmail.com>
2026-07-12 10:20:25 +0530

    feat: initialize backend project structure with Express and Prisma MariaDB integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,53 +1,18 @@
-
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-import {app} from "./app.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './.env'
+    path: "./.env",
 });
 
 connectDB()
-.then (()=>{
-    const port = process.env.PORT || 8000;
-    app.listen(port,()=>{
-        console.log(`server is running at port : ${port}`);
+    .then(() => {
+        const port = process.env.PORT || 8000;
+        app.listen(port, () => {
+            console.log(`Server is running at port: ${port}`);
+        });
     })
-})
-.catch((err)=>{
-    console.log("MONGO db connection failed!!",err);
-})
-
-
-
-
-
-
-
-
-
-
-
-/*
-import express from "express";
-
-const app=express()
-
-( async() => {
-    try {
-        mongoose.connect('${process.env.MONGODB_URL}/${DB_NAME}')
-        app.on("error",(error)=>{
-            console.log("ERR: ",error);
-            throw error
-        })
-
-        app.listen(process.env.PORT,()=>{
-            console.log('App is listening on port ${process.env.PORT}');
-            
-        })
-
-    } catch(error){
-        console.error("ERROR: ",error)
-        throw err
-    }
-})()*/
+    .catch((err) => {
+        console.log("MySQL connection failed!", err);
+    });
